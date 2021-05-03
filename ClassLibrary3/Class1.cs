b548274d01043b0df44dda83087cd40ed8c615e1
Leo0806-studios <66413517+Leo0806-studios@users.noreply.github.com>
2021-05-03 15:09:49 +0200

    Add files via upload

---
+++ ClassLibrary3/Class1.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Unity;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.Rendering;
+namespace ClassLibrary3
+{
+    [KSPAddon]
+    public class Class1 : MonoBehaviour
+    {
+        private static ClassLibrary3.Class1 Instance;
+        public void Awake()
+        {
+             ClassLibrary3.Class1.Instance = this;
+            DontDestroyOnLoad(this);
+    }
+        public void Start() 
+        {
+            
+            Debug.Log("trying to set frameratess");
+            Application.targetFrameRate = 60;
+            Debug.Log("FPS SET TO 60");
+            
+            ScreenMessages.PostScreenMessage("trying to set frameratess", 10f);
+        }
+    }
+}
